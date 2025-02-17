dc32eb54f7488219d97da6dc52c90d492cae2b14
Isaac Moore <isaac.moore@instructure.com>
2025-02-17 08:15:43 -0600

    Create initial page layout, modify config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,38 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded-sm font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <main className="size-full">
+        <div className="p-10 sm:p-40 font-[family-name:var(--font-geist-mono)]">
+          <h2 className="text-2xl font-light mb-6">
+            Usted tiene derechos constitutionales <em>garantizados</em>.
+          </h2>
+          <ol className="list-decimal list-inside">
+            <li className="mb-4">
+              <strong>NO ABRA LA PUERTA</strong> si un agente de inmigración
+              está tocando la puerta.
+            </li>
+            <li className="mb-4">
+              <strong>NO CONTESTE NINGUNA PREGUNTA</strong> de un agente de
+              inmigración si trata de hablar con usted.{" "}
+              <em>Usted tiene el derecho a guardar silencio.</em>
+            </li>
+            <li className="mb-4">
+              <strong>NO FIRME NADA</strong> sin antes hablar con un abogado.{" "}
+              <em>Usted tiene el derecho de hablar con un abogado.</em>
+            </li>
+            <li className="mb-4">
+              Si usted está fuera de su casa, pregúntele al agente si tiene la
+              libertad de irse y si le dice que sí, váyase con tranquilidad.
+            </li>
+            <li className="mb-4">
+              <strong>MUÉSTRALE ESTA TARJETA EL AGENTE.</strong> Si usted está
+              dentro de su casa, muestre la tarjeta por la ventana.
+            </li>
+          </ol>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        {/* define footer */}
       </footer>
     </div>
   );
